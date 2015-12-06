ed8abb1a97586bc3b5baa7be0eefc6756f9d6640
Hal Fulton <rubyhacker@gmail.com>
2015-12-06 16:11:55 -0600

    Albums controller, etc.

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,8 +1,32 @@
 class AlbumsController < ApplicationController
 
+  before_action :set_album, only: [:show, :edit, :update, :destroy]
   def new
     @artists = Artist.all
     @album = Album.new
   end
+  
+  def create 
+    @album = Album.new(album_params)
 
+    respond_to do |format|
+      if @album.save
+        format.html { redirect_to @album, notice: 'Album was successfully created.' }
+        format.json { render :show, status: :created, location: @album }
+      else
+        format.html { render :new }
+        format.json { render json: @album.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_album
+    @album = Album.find(params[:id])
+  end
+
+  def album_params
+    params.require(:album).permit(:title, :artist_id)
+  end
 end
